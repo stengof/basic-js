36f08e568ea4e36936a20fe02550fa50fb11e386
stengof <lmikon007@gmail.com>
2020-02-16 22:19:58 +0300

    feat: make task carbon-dating (2)

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,14 @@
-const MODERN_ACTIVITY= 15; 
+const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample(num) {
+  if ( typeof num !== "string" || num === undefined || num == 0 ) return false;
+
+  num = num.replace(/\./, "X").replace(/\./g, "").replace("X", "."); // If in input "." are more than one delete others "."
+
+  let number = Math.ceil( Math.log( MODERN_ACTIVITY / num) / ( 0.693 / HALF_LIFE_PERIOD ) );
+
+  if ( isNaN(number) || number < 0) return false;
+
+  return number;
 };
