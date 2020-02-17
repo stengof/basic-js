ab5b5faa007df10970573e63623226935b3e35c4
stengof <lmikon007@gmail.com>
2020-02-17 08:09:39 +0300

    feat: make task dream-team (3)

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,13 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(arr) {
+  if ( !Array.isArray(arr) ) return false;
+
+  let newArr = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if ( typeof arr[i] === "string") {
+      newArr.push( arr[i].match(/\b./)[0].toUpperCase() )
+    }
+  }
+
+  return newArr.sort().join('');
 };
