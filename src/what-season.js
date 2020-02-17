d59ba48d8a8b696f3e2e7cff850e3d7c2f6fbedc
stengof <lmikon007@gmail.com>
2020-02-17 19:37:29 +0300

    feat: make task what-season (4)

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,13 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function getSeason(input) {
+  if (!input)  return 'Unable to determine the time of year!';
+
+  if (Object.keys(input).length > 0) throw 'Error';
+
+  let month = input.getMonth();
+  let seasonOfYear = ['winter', 'spring', 'summer', 'autumn'];
+
+  if (month === 11 || month === 0 || month === 1) return seasonOfYear[0];
+  if (month === 2 || month === 3 || month === 4) return seasonOfYear[1];
+  if (month === 5 || month === 6 || month === 7) return seasonOfYear[2];
+  if (month === 8 || month === 9 || month === 10) return seasonOfYear[3];
 };
