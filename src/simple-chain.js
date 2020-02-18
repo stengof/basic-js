b07ec892840193dca40a1781fb34d8978ca74f9d
stengof <lmikon007@gmail.com>
2020-02-18 21:53:33 +0300

    feat: make task simple-chain (7)

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,23 +1,31 @@
 const chainMaker = {
+  chain: [],
   getLength() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    return this.chain.length;
   },
   addLink(value) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    this.chain.push(value);
+    return this;
   },
   removeLink(position) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    if (typeof position != 'number' || position < 1 || position > this.chain.length){
+      this.chain = [];
+      throw "Error";
+    }
+    this.chain.splice(position - 1, 1);
+    return this;
   },
   reverseChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    this.chain.reverse();
+    return this;
   },
   finishChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    let output = this.chain;
+    this.chain = [];
+    for ( let i = 0; i < output.length; i++ ) {
+      if ( output[i] == null ) output[i] = "null";
+    }
+    return `( ${output.join(" )~~( ")} )`;
   }
 };
 
